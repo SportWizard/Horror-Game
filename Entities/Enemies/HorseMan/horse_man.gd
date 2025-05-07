555c20028193b01130537c528a3599bb0f7d3b2b
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-05-07 18:48:32 -0400

    Enemy wander

--- Entities/Enemies/HorseMan/horse_man.gd
+++ Entities/Enemies/HorseMan/horse_man.gd
@@ -1,1 +1,17 @@
 extends AbstractEnemy
+
+func _on_timer_timeout() -> void:
+	self.wander()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	self.wander()
+	
+	# Reset timer
+	$Timer.start(self.wander_timer)
+
+func _physics_process(delta: float) -> void:
+	# Start the timer for wander
+	if $Timer.is_stopped():
+		$Timer.start(self.wander_timer)
+	
+	self.move_and_slide()
--- Entities/Enemies/abstract_enemy.gd
+++ Entities/Enemies/abstract_enemy.gd
@@ -2,11 +2,18 @@ class_name AbstractEnemy extends CharacterBody2D
 
 @export var speed: int = 50
 @export_range(1, 4) var speed_multiplier: int = 2
+@export var wander_timer: float = 3.0
 
 var _cur_dir: Vector2 = Vector2(1, 0) # -1 is left and 1 is right, y is 0
+var _dirs: Array[Vector2] = [Vector2.ZERO, Vector2(-1, 0), Vector2(1, 0), Vector2(0, -1), Vector2(0, 1)] # Rest, left, right, up, down
 
 func wander() -> void:
-	pass
+	var dir: Vector2 = self._dirs[randi() % self._dirs.size()]
+	self.velocity = dir * self.speed
+	
+	# Animations
+	self._cur_dir.x = self._cur_dir.x if Helper.same_axis_dir(self._cur_dir.x, velocity.x) else -self._cur_dir.x
+	$AnimationTree.set("parameters/Idle/blend_position", self._cur_dir)
 
 func chase() -> void:
 	pass
