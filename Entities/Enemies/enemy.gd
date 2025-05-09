91675ebadcc2779869f740c063467d5c749aeef7
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-05-09 13:54:09 -0400

    Separate animation function and activate enemy chase

--- Entities/Enemies/enemy.gd
+++ Entities/Enemies/enemy.gd
@@ -6,17 +6,20 @@ class_name Enemy extends CharacterBody2D
 
 var _cur_dir: Vector2 = Vector2(1, 0) # -1 is left and 1 is right, y is 0
 var _dirs: Array[Vector2] = [Vector2.ZERO, Vector2(-1, 0), Vector2(1, 0), Vector2(0, -1), Vector2(0, 1)] # Rest, left, right, up, down
+var _chasing: Node2D = null
+
+func _animation() -> void:
+	self._cur_dir.x = self._cur_dir.x if Helper.same_axis_dir(self._cur_dir.x, velocity.x) else -self._cur_dir.x
+	$AnimationTree.set("parameters/Idle/blend_position", self._cur_dir)
 
 func _wander() -> void:
 	var dir: Vector2 = self._dirs[randi() % self._dirs.size()]
 	self.velocity = dir * self.speed
 	
-	# Animations
-	self._cur_dir.x = self._cur_dir.x if Helper.same_axis_dir(self._cur_dir.x, velocity.x) else -self._cur_dir.x
-	$AnimationTree.set("parameters/Idle/blend_position", self._cur_dir)
+	self._animation()
 
 func _chase() -> void:
-	pass
+	assert(false, "Enemy class's child \"%s\" should implement function \"_chase\"" % [self.name])
 
 func _attack() -> void:
 	pass
@@ -26,12 +29,13 @@ func _on_wander_timer_timeout() -> void:
 
 func _on_structure_detection_body_entered(body: Node2D) -> void:
 	self._wander()
-	
 	# Reset timer
 	$WanderTimer.start(self.wander_timer)
 
 func _on_player_detection_body_entered(body: Node2D) -> void:
-	print(body.position)
+	self._chasing = body
+	$WanderTimer.stop()
+	self._chase()
 
 func _check_nodes_and_connections(node: Node, node_name: String, node_type: String, connection_name: String, function_name: String):
 	if not node:
@@ -45,8 +49,10 @@ func _ready() -> void:
 	self._check_nodes_and_connections($PlayerDetection, "PlayerDetection", "Area2D", "body_entered", "_on_player_detection_body_entered")
 
 func _physics_process(delta: float) -> void:
+	if self._chasing:
+		self._chase()
 	# Start the timer for wander
-	if $WanderTimer.is_stopped():
+	elif $WanderTimer.is_stopped():
 		$WanderTimer.start(self.wander_timer)
 	
 	self.move_and_slide()
