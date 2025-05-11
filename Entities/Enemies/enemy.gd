9f5c143bf8017ef63825198a2e81799b173acfe7
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-05-10 20:53:27 -0400

    Player stamina

--- Entities/Enemies/enemy.gd
+++ Entities/Enemies/enemy.gd
@@ -1,11 +1,14 @@
 class_name Enemy extends CharacterBody2D
 
-@onready var _state_machine: AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
-
 @export var speed: float = 50
 @export_range(1, 4) var speed_multiplier: float = 2
 @export var wander_time: float = 3.0
 @export var reaction_time: float = 0.25
+
+@onready var _animation_tree: AnimationTree = $AnimationTree
+@onready var _state_machine: AnimationNodeStateMachinePlayback = self._animation_tree["parameters/playback"]
+@onready var _wander_timer: Timer = $WanderTimer
+@onready var _reaction_timer: Timer = $ReactionTimer
 
 var _cur_dir: Vector2 = Vector2(1, 0) # -1 is left and 1 is right, y is 0
 var _dirs: Array[Vector2] = [Vector2.ZERO, Vector2(-1, 0), Vector2(1, 0), Vector2(0, -1), Vector2(0, 1)] # Rest, left, right, up, down
@@ -17,10 +20,10 @@ func _animation() -> void:
 	
 	if self.velocity == Vector2.ZERO:
 		self._state_machine.travel("Idle")
-		$AnimationTree.set("parameters/Idle/blend_position", self._cur_dir)
+		self._animation_tree.set("parameters/Idle/blend_position", self._cur_dir)
 	else:
 		self._state_machine.travel("Walk")
-		$AnimationTree.set("parameters/Walk/blend_position", self._cur_dir)
+		self._animation_tree.set("parameters/Walk/blend_position", self._cur_dir)
 
 func _wander() -> void:
 	var dir: Vector2 = self._dirs[randi() % self._dirs.size()]
@@ -40,11 +43,11 @@ func _on_wander_timer_timeout() -> void:
 func _on_structure_detection_body_entered(body: Node2D) -> void:
 	self._wander()
 	# Reset timer
-	$WanderTimer.start(self.wander_timer)
+	self._wander_timer.start(self.wander_time)
 
 func _on_player_detection_body_entered(body: Node2D) -> void:
 	self._chasing = body
-	$WanderTimer.stop()
+	self._wander_timer.stop()
 	self._chase()
 
 func _on_reaction_timer_timeout() -> void:
@@ -67,12 +70,11 @@ func _ready() -> void:
 	self._check_node_and_connection($ChaseZone, "ChaseZone", "Area2D", "body_exited", "_on_chase_zone_body_exited")
 
 func _physics_process(delta: float) -> void:
-	if self._chasing and $ReactionTimer.is_stopped():
-		$ReactionTimer.start(self.reaction_time)
-	
+	if self._chasing and self._reaction_timer.is_stopped():
+		self._reaction_timer.start(self.reaction_time)
 	# Start the timer for wander
-	elif $WanderTimer.is_stopped():
-		$WanderTimer.start(self.wander_time)
+	elif self._wander_timer.is_stopped():
+		self._wander_timer.start(self.wander_time)
 	
 	self._animation()
 	self.move_and_slide()
--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -2,24 +2,49 @@ extends CharacterBody2D
 
 @export var speed: int = 50
 @export_range(1, 4) var speed_multiplier: int = 2
+@export_range(0, 1e20) var max_stamina: int = 300
+@export var stamina_time: float = 3.0
+@export var stamina_gain: float = 1.0
+
+@onready var _animation_tree: AnimationTree = $AnimationTree
+@onready var _stamina_timer: Timer = $StaminaTimer
+@onready var _progress_bar: ProgressBar = $Camera2D/ProgressBar
 
 var _cur_dir: Vector2 = Vector2(1, 0) # -1 is left and 1 is right, y is 0
+var _cur_stamina = self.max_stamina
+var _gain_stamina = true
 
 func _movement() -> void:
 	var dir: Vector2 = Input.get_vector("Left", "Right", "Up", "Down")
 	self.velocity = dir * self.speed
 	
-	# If the player press sprint, it will 2 times the walking speed
-	if Input.is_action_pressed("Sprint"):
+	if self._cur_stamina > 0 and self.velocity != Vector2.ZERO and Input.is_action_pressed("Sprint"):
 		self.velocity *= self.speed_multiplier
+		
+		if self._gain_stamina:
+			self._gain_stamina = false
+			self._stamina_timer.stop()
+		
+		self._cur_stamina -= 1
+	elif self._cur_stamina < self.max_stamina and self._gain_stamina:
+		self._cur_stamina += 1
+	elif self._stamina_timer.is_stopped():
+		self._stamina_timer.start(self.stamina_time)
 
 func _animation() -> void:
 	if self.velocity.x != 0:
 		self._cur_dir.x = sign(self.velocity.x)
 	
-	$AnimationTree.set("parameters/Idle/blend_position", self._cur_dir)
+	self._animation_tree.set("parameters/Idle/blend_position", self._cur_dir)
+
+func _on_stamina_timer_timeout() -> void:
+	self._gain_stamina = true
+
+func _ready() -> void:
+	self._progress_bar.max_value = self.max_stamina
 
 func _physics_process(delta: float) -> void:
 	self._movement()
 	self._animation()
+	self._progress_bar.value = self._cur_stamina
 	self.move_and_slide()
